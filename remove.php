7a24e4fd3056bae7ce9ef51c620b73a84b148c7b
Ricky Burgin <ricky@burg.in>
2014-04-10 20:46:01 +0100

    Fixed license/credit block formatting

--- remove.php
+++ remove.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
-	This file is part of Obfuscalp.
+    This file is part of Obfuscalp.
 
-	Obfuscalp is free software: you can redistribute it and/or modify
+    Obfuscalp is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -16,12 +16,12 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	
-	Obfuscalp is coded and maintained by Ricky Burgin.
+    Obfuscalp is coded and maintained by Ricky Burgin.
 	
-	© Ricky Burgin 2014
+    © Ricky Burgin 2014
 	
-	http://ricky.burg.in/
-	https://github.com/Orbixx/Obfuscalp
+    http://ricky.burg.in/
+    https://github.com/Orbixx/Obfuscalp
 */
 
 if (empty($argv[1])) die("Usage: php remove.php infected.txt");
